87d6bf5fb6eb071380e009e30718c839b666f66f
shk3029 <shk30290@naver.com>
2022-01-24 22:47:15 +0900

    Initial commit

---
+++ src/main/java/com/example/studycatalogservice/StudyCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.studycatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCatalogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studycatalogservice/StudyCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studycatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
