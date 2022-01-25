008d43c325b9cbfa0fdf46c5de7c284056aaa88d
shk3029 <shk30290@naver.com>
2022-01-25 18:15:51 +0900

    feat: Catalogs 기능 구현 (1)

---
+++ src/main/java/com/example/studycatalogservice/jpa/CatalogRepository.java
@@ -0,0 +1,8 @@
+package com.example.studycatalogservice.jpa;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
+    CatalogEntity findByProductId(String productId);
+}
